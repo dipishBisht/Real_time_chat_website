4b0a2d1fe2774e12d8a0e98436180110116f5d6b
dipishBisht <dipish1718@gmail.com>
2025-03-17 03:54:47 +0530

    final deployment work done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import authRouter from "./routes/auth.route";
 import messageRouter from "./routes/message.route";
 import dotenv from "dotenv";
@@ -6,10 +6,12 @@ import { connectToDB } from "./lib/db";
 import coookieParse from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/utils/socket";
+import path from "path";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 connectToDB();
 
 app.use(express.json({ limit: "10mb" }));
@@ -23,5 +25,13 @@ app.use(cors({
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req: Request, res: Response) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+  });
+}
 
 server.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -4,7 +4,7 @@ import { IUseAuthStore } from "./types";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:8080" : "/";
 
 export const useAuthStore = create<IUseAuthStore>((set, get) => ({
     user: null,
