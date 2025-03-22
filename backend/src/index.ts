38c81d71f75237b19b2948fceb30bf68d540f104
dipishBisht <dipish1718@gmail.com>
2025-03-22 14:57:09 +0530

    changed envirnment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
   app.get("*", (req: Request, res: Response) => {
